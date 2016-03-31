cc8fbbbae6852f670c3d70bf7752e8b576e0d6e7
HeeSeong <HeeSeong@Hee>
2016-03-31 15:19:21 +0900

    ...

--- 7gram/src/bb/member/memberDTO.java
+++ 7gram/src/bb/member/memberDTO.java
@@ -8,6 +8,71 @@ public class memberDTO {
 	private String id;
 	private String name;
 	private String pwd;
+	private String email;
+	private String addr;
+	
+	public memberDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public memberDTO(int idx, String id, String name, String pwd, String email, String addr) {
+		super();
+		this.idx = idx;
+		this.id = id;
+		this.name = name;
+		this.pwd = pwd;
+		this.email = email;
+		this.addr = addr;
+	}
+
+	public int getIdx() {
+		return idx;
+	}
+
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	
 	
 	
 }
